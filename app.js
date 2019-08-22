e90f5dd76b75450932e7ce4576a62f98914ebfa2
mattbra1155 <50120402+mattbra1155@users.noreply.github.com>
2019-08-22 22:37:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-import {playerHealth, enemyHealth} from '/ui.js';
+import {playerHealth, enemyHealth, SCREEN} from '/ui.js';
 import {items} from '/items.js';
 import {level} from '/levels.js';
 import {persons} from '/characters.js';
 import {attack} from '/inputs.js'
 
+
 console.log(persons)
 console.log(items)
 console.log(level)
 console.log(attack)
+console.log(SCREEN)
+
 
 
 // INIT
@@ -16,4 +19,11 @@ let updatePersonHealth = () => {
     playerHealth.textContent = persons.player.hp;
     enemyHealth.textContent = level.monster.hp;
 }
+
+// Shows health score
 updatePersonHealth();
+
+// Shows start screen
+SCREEN.startScreen();
+
+
