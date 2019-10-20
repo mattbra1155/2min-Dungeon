d4ff4e6bd9e5685a09038ca8cdb12f92cc9253ad
Mateusz Brania <mattbra1155@gmail.com>
2019-10-20 20:04:51 +0200

    added a global object

--- inputs.js
+++ inputs.js
@@ -1,13 +1,8 @@
-import {level} from '/levels.js';
-import {persons} from '/characters.js';
-import {screen} from '/ui.js';
+import {level, persons} from '/index.js';
 
 
 /// INPUTS
 const attackButton = document.querySelector('#attackButton');
-const turnButton = document.querySelector('#turnButton');
-const inventoryButton = document.querySelector('#inventoryButton');
-const inventoryItem = document.querySelectorAll('.inventoryItem');
 
 const attack = attackButton.addEventListener('click', e => {
     e.preventDefault;
