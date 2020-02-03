2c220e6d5462fbaac680d09f619afc8e4ff07b4f
Mateusz Brania <mattbra1155@gmail.com>
2020-02-03 12:39:43 +0100

    fixin pathnames

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import {items} from '/items.js'
+import {items} from './items.js'
 
 
-export * from '/characters.js';
-export * from '/inputs.js';
-export * from '/items.js';
-export * from '/levels.js';
-export * from '/ui.js';
-export * from '/global.js';
-export * from '/turn.js';
+export * from './characters.js';
+export * from './inputs.js';
+export * from './items.js';
+export * from './levels.js';
+export * from './ui.js';
+export * from './global.js';
+export * from './turn.js';
