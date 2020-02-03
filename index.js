9032388ab29180ffb526e5b5961b217e397c1c88
Mateusz Brania <mattbra1155@gmail.com>
2020-02-03 12:55:52 +0100

    fixed pathname

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-    import {items} from '/2min-Dungeon/items.js'
-
-
+export * from '/2min-Dungeon/items.js';
 export * from '/2min-Dungeon/characters.js';
 export * from '/2min-Dungeon/inputs.js';
-export * from '/2min-Dungeon/items.js';
 export * from '/2min-Dungeon/levels.js';
 export * from '/2min-Dungeon/ui.js';
 export * from '/2min-Dungeon/global.js';
